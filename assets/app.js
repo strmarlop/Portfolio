c71b71c44ceb45be89c8516650daccfb1b226708
Esther Martinez <estmartinezl@gmail.com>
2023-07-20 21:06:48 +0200

    created about me, without footer

--- assets/app.js
+++ assets/app.js
@@ -4,6 +4,45 @@
  * We recommend including the built version of this JavaScript file
  * (and its CSS file) in your base layout (base.html.twig).
  */
+var items = document.querySelectorAll(".timeline li");
+
+function isElementInViewport(el) {
+  var rect = el.getBoundingClientRect();
+  return (
+    rect.top >= 0 &&
+    rect.left >= 0 &&
+    rect.bottom <= (window.innerHeight || document.documentElement.clientHeight) &&
+    rect.right <= (window.innerWidth || document.documentElement.clientWidth)
+  );
+}
+
+function callbackFunc() {
+  for (var i = 0; i < items.length; i++) {
+    if (isElementInViewport(items[i])) {
+      if(!items[i].classList.contains("in-view")){
+        items[i].classList.add("in-view");
+      }
+    } else if(items[i].classList.contains("in-view")) {
+        items[i].classList.remove("in-view");
+    }
+  }
+}
+ 
+window.addEventListener("load", callbackFunc);
+window.addEventListener("scroll", callbackFunc);
+
+
+
+
+
+
+
+
+
+
+
+
+
 import "bootswatch/dist/simplex/bootstrap.min.css";
 
 // any CSS you import will output into a single css file (app.css in this case)
