0ac97fdb40155d398005dc4bb22f6877805e9213
Esther Martinez <estmartinezl@gmail.com>
2023-07-31 21:46:50 +0200

    add last projects and favicon

--- migrations/Version20230731191922.php
+++ migrations/Version20230731191922.php
@@ -10,7 +10,7 @@ use Doctrine\Migrations\AbstractMigration;
 /**
  * Auto-generated Migration: Please modify to your needs!
  */
-final class Version20230720115447 extends AbstractMigration
+final class Version20230731191922 extends AbstractMigration
 {
     public function getDescription(): string
     {
